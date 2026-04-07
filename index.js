df404027a6ce1108be5574379c7d7119f046f1f8
Weston Hopkins <whopkins@anamosa.k12.ia.us>
2026-04-07 16:55:58 +0000

    quinns an AI cheater

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function calculateTotal(subtotal, shipping = 2.50) {
+    return subtotal + shipping
+}
+function printTopThreeHeadlines(...headlines){
+    return headlines.slice(0, 3).join("\n");
+}
